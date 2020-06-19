ccaa65d5249c532bdfebeccadff08361f410bbc4
Drew Beckmen <drew.beckmen@yale.edu>
2020-06-18 23:25:28 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Alternative solution for the route that maps to StudentController Index Action
+  # get 'students', to: 'students#index'
+  
+  # Also maps to index action in StudentController
+  resources :students, only: :index 
 end
